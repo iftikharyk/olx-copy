5ce9692243b46cd29e28dfc74c27b758500a042a
Iftikhar Khan <iftikhar.khan@sixlogs.net>
2019-03-27 10:19:21 +0500

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,30 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { MobilesComponent } from './pages/mobiles/mobiles.component';
+import { VechiclesComponent } from './pages/vechicles/vechicles.component';
+import { PropertyForSaleComponent } from './pages/property-for-sale/property-for-sale.component';
+import { PropertyForRentComponent } from './pages/property-for-rent/property-for-rent.component';
+import { ElectronicsComponent } from './pages/electronics/electronics.component';
+import { BikesComponent } from './pages/bikes/bikes.component';
+import { BusinessComponent } from './pages/business/business.component';
+import { ServicesComponent } from './pages/services/services.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    MobilesComponent,
+    VechiclesComponent,
+    PropertyForSaleComponent,
+    PropertyForRentComponent,
+    ElectronicsComponent,
+    BikesComponent,
+    BusinessComponent,
+    ServicesComponent
   ],
   imports: [
     BrowserModule,
