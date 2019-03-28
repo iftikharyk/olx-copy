844bf87151dfb0140db6516dd7ef5011c4e4d87d
Iftikhar Khan <iftikhar.khan@sixlogs.net>
2019-03-28 18:09:06 +0500

    home page almost done

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -7,9 +7,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
+  // tslint:disable-next-line:no-inferrable-types
+  // show: boolean = false;
+  // toggler: any = 'Show';
+
   constructor() { }
 
   ngOnInit() {
   }
+
+  // toggleCategories() {
+  //   this.show = !this.show;
+
+  //   if (this.show) {
+  //     this.toggler = 'Hide';
+  //   } else {
+  //     this.toggler = 'show';
+  //   }
+  // }
 
 }
